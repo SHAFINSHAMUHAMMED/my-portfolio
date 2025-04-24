08e83826f3eb3d0329dbf04b31bdd3a15b23f419
webq-media <webqmedia@gmail.com>
2025-04-24 15:52:51 +0530

    favi

--- src/data/data.js
+++ src/data/data.js
@@ -3,7 +3,7 @@
 export const data = [
   {
     id: 1,
-    img: 'https://i.postimg.cc/63jRQZPS/Chat-GPT-Image-Apr-24-2025-12-54-39-PM.png',
+    img: 'https://i.postimg.cc/Jnzx5g1N/Chat-GPT-Image-Apr-24-2025-03-35-10-PM-1-2.png',
     title: "Convocation Registration Portal",
     desc: "Engineered a convocation registration portal with secure online payment integration and real-time seat-selection booking for attendees. Supplied an admin dashboard to manage applications, track payments, and monitor seating capacity at a glance.",
     link: "https://convocation.learnersuae.com/",
